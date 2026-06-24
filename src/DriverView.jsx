d8a912109835287b46d077e8b7d87894358ed13c
Botbot Lulu Shamriz <botbotlulushamriz@Omris-Mac-Mini.local>
2026-06-24 13:07:27 +0300

    fix: correctly close CircleMarker tag on line 209

--- src/DriverView.jsx
+++ src/DriverView.jsx
@@ -206,7 +206,7 @@ export default function DriverView() {
 
               </div>
             </Popup>
-          </Marker>
+          </CircleMarker>
         ))}
       </MapContainer>
     </div>
